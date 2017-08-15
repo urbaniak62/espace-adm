f4416d10780417ef9d8d936670bb0ae85cc915bf
urbaniak <urbaniaknicolas62@gmail.com>
2017-08-15 16:17:24 +0200

    base HTML JS

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+    var val = document.getElementById('valider').onclick = function(){
+    alert("yes");
+    var produit=document.getElementsByTagName("input")[0].value;
+    var phrase=document.getElementsByTagName("input")[1].value;
+    var description=document.getElementsByTagName("input")[2].value;
+
+    alert(produit +"-----" + phrase + "-------" + description);
+    alert(produit);
+
+    // var a = document.getElementsById("a");
+    // var b = document.getElementsById("b");
+    // var c = document.getElementsById("c");
+
+    a.append=produit[0].value;
+}
