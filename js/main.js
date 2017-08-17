b342ae7925713c48be8bb05d42231292d09ed803
urbaniak <urbaniaknicolas62@gmail.com>
2017-08-17 16:14:24 +0200

    js

--- js/main.js
+++ js/main.js
@@ -4,13 +4,20 @@
     var produit=document.getElementsByTagName("input")[0].value;
     var phrase=document.getElementsByTagName("input")[1].value;
     var description=document.getElementsByTagName("input")[2].value;
+    var array=[produit,phrase,description];
+    // return array;
+    alert(array);
 
-    alert(produit +"-----" + phrase + "-------" + description);
-    alert(produit);
+    //  alert(produit +"-----" + phrase + "-------" + description);
+    //  alert(produit);
+  }
 
-    // var a = document.getElementsById("a");
-    // var b = document.getElementsById("b");
-    // var c = document.getElementsById("c");
-
-    a.append=produit[0].value;
-}
+  // function recup(array){
+  //   this.produit=array[0],
+  //   this.phrase=array[1],
+  //   this.description=array[2],
+  //   this.getName=function(){
+  //
+  //     var fiche = document.getElementsByClassName("fiche");
+  //     fiche.append("<p>" + this.produit + "</p>"  );
+// }
