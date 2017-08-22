4ca422a397996ae56350c08dab6cb8ad1084bf53
urbaniak <urbaniaknicolas62@gmail.com>
2017-08-22 14:00:37 +0200

    affichage des inputs

--- js/main.js
+++ js/main.js
@@ -4,20 +4,10 @@
     var produit=document.getElementsByTagName("input")[0].value;
     var phrase=document.getElementsByTagName("input")[1].value;
     var description=document.getElementsByTagName("input")[2].value;
-    var array=[produit,phrase,description];
-    // return array;
-    alert(array);
 
-    //  alert(produit +"-----" + phrase + "-------" + description);
-    //  alert(produit);
-  }
+     $(".fiche").append("<p> votre produit est : "
+      + produit + "</p><p>votre phrase d'accroche est : "
+      + phrase + "</p><p>votre description est : "
+      + description + "</p>" );
 
-  // function recup(array){
-  //   this.produit=array[0],
-  //   this.phrase=array[1],
-  //   this.description=array[2],
-  //   this.getName=function(){
-  //
-  //     var fiche = document.getElementsByClassName("fiche");
-  //     fiche.append("<p>" + this.produit + "</p>"  );
-// }
+  };
