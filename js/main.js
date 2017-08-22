940d04c2f4957ead34d51bd1481a2bed2d5cfb86
urbaniak <urbaniaknicolas62@gmail.com>
2017-08-22 15:08:15 +0200

    delete des inputs

--- js/main.js
+++ js/main.js
@@ -5,9 +5,11 @@
     var phrase=document.getElementsByTagName("input")[1].value;
     var description=document.getElementsByTagName("input")[2].value;
 
+      $("p").empty();
      $(".fiche").append("<p> votre produit est : "
       + produit + "</p><p>votre phrase d'accroche est : "
       + phrase + "</p><p>votre description est : "
       + description + "</p>" );
+      $('input').val('');
 
   };
